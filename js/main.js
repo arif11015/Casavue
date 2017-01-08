36b58cfd04a582ea6049ca6eda10c988def623c2
Arif Rahman <xossarif@gmail.com>
2017-01-09 00:40:16 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	
+	$('#navigation').slimmenu(
+{
+    resizeWidth: '800',
+    collapserTitle: 'Main Menu',
+    animSpeed: 'medium',
+    easingEffect: null,
+    indentChildren: false,
+    childrenIndenter: '&nbsp;'
+});
+
+});
